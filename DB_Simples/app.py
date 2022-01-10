f17e596bfc3c9052342c2af8791eb1e2e3962b36
Erivan Serafim <erivan_serafim_fernandes@hotmail.com>
2022-01-09 22:49:16 -0300

    Finalizando DB_Simples

--- DB_Simples/app.py
+++ DB_Simples/app.py
@@ -1,11 +1,14 @@
 from enum import unique
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
+
+from sqlalchemy.orm import backref
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = "secret"
 
 db = SQLAlchemy(app)
 
@@ -13,6 +16,16 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(84), nullable=False, unique=True)
+    pokemons = db.relationship('Pokemon', backref='user')
+
+    def __str__(self):
+        return self.name
+
+class Pokemon(db.Model):
+    __tablename__ = "pokemons"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(84), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
     def __str__(self):
         return self.name
@@ -29,6 +42,8 @@ def index():
 def cria_db():
     db.create_all()
     
+    flash('app.db criado com sucesso!', 'success')
+
     return redirect(url_for('index'))
 
 @app.route("/user/info/<int:id>")
@@ -45,13 +60,34 @@ def add(name):
     db.session.add(user)
     db.session.commit()
 
+    flash('usuário adicionado com sucesso!', 'success')
+
     return redirect(url_for('index'))
+
+@app.route("/pokemon/add/<name_user>-<name_pokemon>")
+def add_pokemon(name_user, name_pokemon):
+    user = User.query.filter_by(name=name_user).first()
+    if user:
+        pokemon=Pokemon()
+        pokemon.name=name_pokemon
+        pokemon.user_id=user.id
+        db.session.add(pokemon)
+        db.session.commit()
+
+        flash('pokemon adicionado com sucesso!', 'success')
+    else:
+        flash('Usuário não encontrado! Pokemon não adicionado!', 'danger')
+
+    return redirect(url_for('index'))
+
 
 @app.route("/user/delete/<int:id>")
 def delete(id):
     user = User.query.filter_by(id=id).first()
     db.session.delete(user)
     db.session.commit()
+
+    flash('Usuário deletado com suceso!', 'danger')
 
     return redirect(url_for('index'))
 
--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 #### Aplicativos desenvolvidos ####
 
-1) Hello World: 
-    Aplicativo simples com apenas uma tela de Hello World
+<ul>
+    <b>Hello_World</b>
+    <li>Objetivo: Entender o básico do básico</li>
+    <li>Funcionamento: Apenas uma tela de Hello World</li> 
+<ul>
+<ul>
+    <b>DB_Simples</b>
+    <li>Objetivo: Entender como usar db no flask</li>
+    <li>Funcionamento: Cria um database com duas tabelas "Users" e "Pokemons". É possivel criar usuários e atribuir pokemons a esses usuários</li> 
+<ul>
 
 #### Rodando os apps ####
 
