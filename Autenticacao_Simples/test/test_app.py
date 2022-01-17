16c0f5cfb1ae1e4f656a102b77e87ea4e11c4ffe
Erivan Serafim <erivan_serafim_fernandes@hotmail.com>
2022-01-16 23:45:12 -0300

    Incluindo testes de autenticação

--- Autenticacao_Simples/test/test_app.py
+++ Autenticacao_Simples/test/test_app.py
@@ -1,18 +1,49 @@
 import pytest
-from app import create_app
+from app import create_app, db
+from app.models import User
 
 @pytest.fixture
 def client():
     app = create_app()
     app.config['TESTING'] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"
+    app.config['WTF_CSRF_ENABLED'] = False
 
     context = app.app_context()
     context.push()
 
+    db.create_all()
+
     yield app.test_client()
+
+    db.session.remove()
+    db.drop_all()
 
     context.pop()
 
 def test_se_a_pagina_login_retorna_status_code_200(client):
     response = client.get("/login")
     assert response.status_code == 200
+
+def test_se_o_link_de_register_existe(client):
+    response = client.get("/login")
+    assert "Register" in response.get_data(as_text=True)
+
+def test_registrando_usuario(client):
+    data = {
+        'name': 'Zezinho',
+        'email': 'Zezinho@teste.com',
+        'pwd': '123'
+    }
+    response = client.post('/register', data=data, follow_redirects=True)
+    assert "Usuário cadastrado com sucesso!" in response.get_data(as_text=True)
+
+def test_register_e_logando_usuario(client):
+    data = {
+        'name': 'Zezinho',
+        'email': 'Zezinho@teste.com',
+        'pwd': '123'
+    }
+    client.post('/register', data=data, follow_redirects=True)
+    response = client.post('/login', data=data, follow_redirects=True)
+    assert "Sair" in response.get_data(as_text=True)
